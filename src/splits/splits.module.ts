69bb2923309edb552d208d1f27f9428c71eb0578
shashvatsawar <shashvatsawar@gmail.com>
2024-02-17 20:59:40 +0530

    Added Resources

--- src/splits/splits.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SplitsService } from './splits.service';
-import { SplitsController } from './splits.controller';
-
-@Module({
-  providers: [SplitsService],
-  controllers: [SplitsController]
-})
-export class SplitsModule {}
